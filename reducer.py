193a99bcfae366b2107b17607c8e214424499c7a
artunit <art632000@yahoo.ca>
2012-05-28 22:08:59 -0400

    handle blank lines in hadoop results

--- reducer.py
+++ reducer.py
@@ -5,10 +5,14 @@ import sys
 # input comes from STDIN for hadoop processing
 for line in sys.stdin:
     line = line.strip()
-    char_cnt, filename, word, x0, y0, x1, y1 = line.split('\t', 7)
-    x0 = int(x0)
-    y0 = int(y0)
-    x1 = int(x1)
-    y1 = int(y1)
+    if len(line) > 0:
+       try:
+          char_cnt, filename, word, x0, y0, x1, y1 = line.split('\t', 7)
+          x0 = int(x0)
+          y0 = int(y0)
+          x1 = int(x1)
+          y1 = int(y1)
 
-    print "%s %s %d %d %d %d" % (filename,word,x0,y0,x1,y1)
+          print "%s %s %d %d %d %d" % (filename,word,x0,y0,x1,y1)
+       except:
+          line  = ''
